9f3fb9a614ba32f8abe93c0b10d3a3d0811dcbeb
gonnbae <62999010+gonnbae@users.noreply.github.com>
2020-04-19 04:42:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -41,6 +41,7 @@ async def on_member_join(member):
 async def on_message(message):
     if message.content.startswith('対戦募集')
         await message.channel.send(f'<@&701098449864622091>{message.author.name}さんが対戦募集しています！')
+        await bot.process_commands(message)
 
 
 bot.run(token)
