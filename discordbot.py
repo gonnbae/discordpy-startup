10bfbe9b47d9f527812d86068a33298f37a4b6bd
gonnbae <62999010+gonnbae@users.noreply.github.com>
2020-04-19 01:56:22 +0900

    matching

--- discordbot.py
+++ discordbot.py
@@ -32,6 +32,58 @@ async def on_member_join(member):
     await channel.send('こんにちは、' +str(member.mention)+
                        'さん！よろしくお願いします！👍')
     
+MATCH_CHANNEL_ID =701111381633531905
+STARVED_MATCHING =<@&701098449864622091>
 
+# discordAPIモジュール
+from discord import message
+from discord import client
+from discord import channel
+
+# 自作モジュール
+from MyMessageClass.message_maker import MessageMaker
+from nessmado_discord_manager import ChannelManager
+
+
+class AnnounceMatchMessageMaker(MessageMaker):
+    def __init__(self):
+        super(AnnounceMatchMessageMaker, self).__init__()
+        self.keyword = '対戦募集'
+        self.output_replies = []
+        self.message_pattern = 0
+        self.ch_manager = ChannelManager()
+        self.keychannel = self.ch_manager.MATCH_CHANNEL_ID
+        print(type(self.keychannel))
+        self.starved_matching = self.ch_manager.STARVED_MATCHING
+
+    async def _makeMessage(self, message, client, channel=None) -> str:
+        asyncio_result = None
+        if self.message_pattern == -1:
+            return asyncio_result
+        if self.message_pattern == 0:
+            self.reply = f'{message.author.mention} さんが対戦募集を開始しました。 {self.starved_matching}\n \
+                参加したい方はこちらから→{message.channel.mention}  \n'
+        self.output_replies.append(
+            [client.get_channel(self.ch_manager.MATCH_CHANNEL_ID), self.reply])
+        for reply_channel, reply_content in self.output_replies:
+            asyncio_result = await reply_channel.send(reply_content)
+        return asyncio_result
+
+    async def executeFunction(self, message, client) -> str:
+        asyncio_result = None
+        # 「対戦募集」から始まってなかったら -1 パターンのメッセージを作成
+        if not message.content.startswith(self.keyword):
+            self.message_pattern = -1
+            asyncio_result = await self._makeMessage(message, client)
+            return asyncio_result
+        # 対戦募集チャンネル「以外」でのメッセージはスルーする。
+        if message.channel.id == self.keychannel:
+            asyncio_result = await self._makeMessage(message, client)
+        return asyncio_result
+
+    def checkTriggers(self, message) -> bool:
+        if self._checkKeyword(message) or self._checkChannelMessageWritten(message):
+            return True
+        return False
 
 bot.run(token)
