163144dccab6088b278af8e69fbc881fdef65917
gonnbae <62999010+gonnbae@users.noreply.github.com>
2020-04-28 11:33:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,6 +12,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 
